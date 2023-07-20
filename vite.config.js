a0c1cfba7c1a057fc1085fd592be00697a9c048a
mehdikord <mehdi.kord171@gmail.com>
2023-07-20 15:13:39 +0330

    upgrade vite

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,14 @@ export default defineConfig({
     plugins: [
         laravel(
             {
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+                input: ['resources/css/app.css', 'resources/js/front.js'],
+                refresh: true,
             },
             {
-                input: ['resources/css/app.css', 'resources/js/app.js'],
-                refresh: true,
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
             }
+
         ),
         vue({
             template :{
