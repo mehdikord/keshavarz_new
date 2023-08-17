d9641d6287617e7c6ff2882e86b1b1a41a49d7db
kowsarkarimi <kowsarkarimi7979@gmail.com>
2023-08-18 00:45:16 +0330

    update

---
+++ database/migrations/2023_08_18_002919_add_fields_into_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->longText('bio')->nullable()->after('profile');
+            $table->boolean('is_provider')->default(0)->after('bio');
+            $table->boolean('is_customer')->default(0)->after('is_provider');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
