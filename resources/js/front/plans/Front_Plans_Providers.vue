80b695d6087f09609988442d979226ecf45d4c5a
mehdikord <mehdi.kord171@gmail.com>
2023-08-21 22:17:34 +0330

    update

--- resources/js/front/plans/Front_Plans_Providers.vue
+++ resources/js/front/plans/Front_Plans_Providers.vue
@@ -53,23 +53,97 @@ export default {
                         خرید {{plan.title}}
                     </q-card-section>
                     <q-card-section class="q-px-xs">
-                        <q-banner class="bg-teal-10 text-white" rounded>
-                            <q-icon name="fas fa-info-circle fa-2x q-mr-sm"></q-icon>
-                            <span class="invoice-text">
+                        <template v-if="this.AuthUserCheck()">
+                            <q-banner class="bg-teal-10 text-white" rounded>
+                                <q-icon name="fas fa-info-circle fa-2x q-mr-sm"></q-icon>
+                                <span class="invoice-text">
                                 کاربر گرامی بعد از خرید اشتراک ، خدمات اپلیکیشن کشاورز بلافاصله برای شما به صورت خودکار فعال میشود .
                             </span>
-                        </q-banner>
-                        <div class="q-mt-md">
-                            <div class="invoice-info-title text-red text-center">
-                                اطلاعات پرداخت
-                            </div>
-                            <q-separator class="q-mt-sm"/>
-                            <div class="q-mt-sm row justify-center">
-                                <div class="col-lg-7">
+                            </q-banner>
+                            <div class="q-mt-md">
+                                <div class="invoice-info-title text-red text-center">
+                                    اطلاعات پرداخت
+                                </div>
+                                <q-separator class="q-mt-sm"/>
+                                <div class="q-mt-sm row justify-center">
+                                    <div class="col-lg-7 text-center row">
+                                        <div class="col-6">
+                                            <span>عنوان محصول : </span>
+                                        </div>
+                                        <div class="col-6">
+                                        <span class="text-green-8">
+                                            {{plan.title}}
+                                        </span>
+                                        </div>
+                                        <div class="col-12 q-mt-md q-mb-md">
+                                            <q-separator/>
+                                        </div>
+                                        <div class="col-6">
+                                            <span>نوع پرداخت : </span>
+                                        </div>
+                                        <div class="col-6">
+                                        <span class="text-green-8">
+                                            پرداخت آنلاین شبکه شتاب
+                                        </span>
+                                        </div>
+                                        <div class="col-12 q-mt-md q-mb-md">
+                                            <q-separator/>
+                                        </div>
+                                        <div class="col-6">
+                                            <span>پذیرنده : </span>
+                                        </div>
+                                        <div class="col-6">
+                                        <span class="text-green-8">
+                                            شرکت کیانمهر
+                                        </span>
+                                        </div>
+                                        <div class="col-12 q-mt-md q-mb-md">
+                                            <q-separator/>
+                                        </div>
+                                        <div class="col-6">
+                                            <span>مبلغ قابل پرداخت : </span>
+                                        </div>
+                                        <div class="col-6">
+                                            <template v-if="plan.is_free">
+                                                <strong class="text-green-8">
+                                                    رایگان
+                                                </strong>
+                                            </template>
+                                            <template v-else>
+                                                <strong v-if="plan.sale" class="text-indigo">
+                                                    {{this.$filters.numbers(plan.sale)}}
+                                                </strong>
+                                                <strong v-else class="text-indigo">
+                                                    {{this.$filters.numbers(plan.price)}}
+                                                </strong>
+                                                <span class="q-ml-sm">تومان</span>
+                                            </template>
 
+                                        </div>
+                                        <div class="col-12 q-mt-md q-mb-md">
+                                            <q-separator/>
+                                        </div>
+                                        <div class="col-12">
+                                            <q-img src="/logo_name.png" class="invoice-img"></q-img>
+                                        </div>
+                                        <div class="col-12 q-mt-md q-mb-md text-center">
+                                            <q-btn color="green-7" glossy>پرداخت نهایی و فعالسازی</q-btn>
+                                        </div>
+
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </template>
+                        <template v-else>
+                            <q-banner class="bg-red text-center text-white">
+                                <span>کاربر گرامی برای خرید و فعالسازی اشتراک ابتدا باید وارد حساب کاربری خود شوید</span>
+                            </q-banner>
+                            <div class="q-mt-lg q-mb-md text-center">
+                                <q-btn :to="{name : 'profile'}" color="indigo" glossy>ثبت نام / ورود به حساب کاربری</q-btn>
+                            </div>
+                        </template>
+
+
                     </q-card-section>
                 </q-card>
             </q-dialog>
@@ -98,6 +172,9 @@ export default {
     font-size: 16px;
     font-weight: 600;
 }
+.invoice-img{
+    width: 110px;
+}
 
 @media only screen and (max-width: 600px) {
     .box-padding{
@@ -114,6 +191,9 @@ export default {
     }
     .invoice-text{
         font-size: 12px;
+    }
+    .invoice-img{
+        width: 70px;
     }
 
 }
