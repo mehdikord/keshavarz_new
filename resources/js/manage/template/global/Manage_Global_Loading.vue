6f696b358e72eb91dbee03f8ec203fdf2be61846
mehdikord <mehdi.kord171@gmail.com>
2023-09-08 15:45:11 +0330

    update

--- resources/js/manage/template/global/Manage_Global_Loading.vue
+++ resources/js/manage/template/global/Manage_Global_Loading.vue
@@ -4,7 +4,7 @@
             color="indigo-10"
             size="75px"
         />
-        <h6 class="text-center q-mt-md">Loading items ...</h6>
+        <h6 class="text-center q-mt-md">درحال بارگذاری ...</h6>
     </div>
 </template>
 
