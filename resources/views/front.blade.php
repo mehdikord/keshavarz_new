b69cedf617fc73f36c110081c866bab621726835
mehdikord <mehdi.kord171@gmail.com>
2023-09-25 20:56:51 +0330

    update

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -3,12 +3,20 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="enamad" content="860688" >
 
     <title>860688</title>
     <link rel="icon" href="logo.png">
     <link rel="stylesheet" href="{{asset('front/css/font.css')}}">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>My title here</title>
+    <meta name="description" content="My description here">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180">
+    <link rel="mask-icon" href="/mask-icon.svg" color="#FFFFFF">
+    <meta name="theme-color" content="#ffffff">
+
+    <script src="{{asset('src-pwa/register-service-worker.js')}}"></script>
 
     @vite(['resources/js/front.js'])
 </head>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 export default defineConfig({
@@ -16,6 +17,32 @@ export default defineConfig({
 
             }
         }),
-        quasar()
+        quasar(),
+        VitePWA({
+            workbox: {
+                cleanupOutdatedCaches: false,
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+
+            },
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            manifest:{
+                name : 'Keshavarz',
+                description : 'test',
+                theme_color : '#ffffff',
+                background_color : '#ffffff',
+                display : 'standalone',
+                icons : [
+                    {
+                        src : "/logo.pnd",
+                        sizes : '512x512',
+                        type : 'image/png'
+                    }
+
+                ]
+
+
+            }
+        }),
     ],
 });
