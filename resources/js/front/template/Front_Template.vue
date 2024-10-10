93bfbadd7a31341a08620c7625c7240494f65f3a
Mehdi Kord <mehdi.kord171@gmail.com>
2024-10-10 19:10:21 +0330

    upgrade

--- resources/js/front/template/Front_Template.vue
+++ resources/js/front/template/Front_Template.vue
@@ -11,6 +11,34 @@
         </q-page-container>
 
         <Front_Template_Footer/>
+        <q-dialog v-model="app_dialog" position="standard">
+            <q-card style="width: 100%!important;">
+                <q-card-section>
+                    <div class="text-center">
+                        <strong class="text-green-6 font-iransans font-17">نصب اپلیکیشن کشاورز در موبایل</strong>
+                        <div class="q-mt-sm font-13 text-grey-8 ">
+                            <strong>برای دسترسی سریعتر و کارایی بهتر اپلیکیشن کشاورز را در موبایل خود نصب کنید</strong>
+                        </div>
+                        <div class="q-mt-lg row">
+                            <div class="col-6 q-pa-sm">
+                                <q-btn style="width: 100%" color="green-8" glossy class="font-16 " label="نصب کشاورز"></q-btn>
+
+                            </div>
+                            <div class="col-6 q-pa-sm">
+                                <q-btn style="width: 100%" color="grey-8" glossy class="font-16 " label="بستن"></q-btn>
+
+                            </div>
+                        </div>
+                    </div>
+                </q-card-section>
+
+            </q-card>
+        </q-dialog>
+
+
+
+
+
     </q-layout>
 
 </template>
@@ -30,6 +58,11 @@ export default defineComponent({
         Front_Template_Footer,
         Front_Template_Header
 
+    },
+    data(){
+        return{
+            app_dialog:true,
+        }
     }
 
 
--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -44,30 +44,30 @@
     }
 
 
-    let deferredPrompt;
-
-    window.addEventListener('beforeinstallprompt', (e) => {
-        // جلوگیری از نمایش خودکار پیشنهاد نصب
-        e.preventDefault();
-        deferredPrompt = e;
-
-        // نمایش یک دکمه برای نصب برنامه
-        const installButton = document.getElementById('installButton');
-        installButton.style.display = 'block';
-
-        installButton.addEventListener('click', () => {
-            installButton.style.display = 'none';
-            deferredPrompt.prompt();
-            deferredPrompt.userChoice.then((choiceResult) => {
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the install prompt');
-                } else {
-                    console.log('User dismissed the install prompt');
-                }
-                deferredPrompt = null;
-            });
-        });
-    });
+    // let deferredPrompt;
+    //
+    // window.addEventListener('beforeinstallprompt', (e) => {
+    //     // جلوگیری از نمایش خودکار پیشنهاد نصب
+    //     e.preventDefault();
+    //     deferredPrompt = e;
+    //
+    //     // نمایش یک دکمه برای نصب برنامه
+    //     const installButton = document.getElementById('installButton');
+    //     installButton.style.display = 'block';
+    //
+    //     installButton.addEventListener('click', () => {
+    //         installButton.style.display = 'none';
+    //         deferredPrompt.prompt();
+    //         deferredPrompt.userChoice.then((choiceResult) => {
+    //             if (choiceResult.outcome === 'accepted') {
+    //                 console.log('User accepted the install prompt');
+    //             } else {
+    //                 console.log('User dismissed the install prompt');
+    //             }
+    //             deferredPrompt = null;
+    //         });
+    //     });
+    // });
 
 
 
